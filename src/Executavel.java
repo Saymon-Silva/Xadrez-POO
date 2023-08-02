cc2a18bccfc656afcf4c9acea68435e3f13164b8
saymon_p_silva <saymonsilv401@gmail.com>
2023-08-02 16:49:27 -0300

    COMITA LEK, PRECISA AARUKR A INTERFACE E A CIAÇÃO DE PERSONAGEM, JUNTO DA ESCOLHA DO PERSONAGEM NA HORA DA BATALHA KAKAKAKA TMNC SO A MENTE DEO PALHAÇO

--- src/Executavel.java
+++ src/Executavel.java
@@ -5,26 +5,33 @@ public class Executavel {
 
     public static void main(String[] args) {
 
+
         Scanner sc = new Scanner(System.in);
 
         Jogador j1 = new Jogador("jorge","Senh@123");
         Jogador j2 = new Jogador("Wilson","wilson");
 
         Tabuleiro tabuleiro = new Tabuleiro();
+        printarTabuleiro(tabuleiro);
+
 
         j1.setCor("Branco",tabuleiro);
         j2.setCor("Preto", tabuleiro);
 
-        System.out.println(j1.getPecas());
+//        System.out.println(j1.getPecas()); // printando as peças do jogador
+//        System.out.println(j2.getPecas());
 
         int escolhaPeca = sc.nextInt();
+
         // Escolha da peça
         Peca peca = j1.getPecas().get(escolhaPeca);
         System.out.println(peca);
 
         //Escolha da posição para o movimento
         ArrayList<Posicao> posicoes = peca.possiveisMovimento(tabuleiro);
-        System.out.println("posicoes "+posicoes);
+        for(int i = 0; i <= posicoes.size(); i++) {
+            System.out.println("posicoes " + posicoes);
+        }
         int escolhaPosicao =sc.nextInt();
         Posicao posicao = posicoes.get(escolhaPosicao);
 
@@ -53,21 +60,18 @@ public class Executavel {
     }
     private static void printarTabuleiro( Tabuleiro tabuleiro){
 
-        ArrayList<Posicao> posicaoPecasTabuleiro = tabuleiro.getPosicoes();
+        ArrayList<Posicao> posicao = tabuleiro.getPosicoes();//posicao do tabuleiro
 
+      for( int i = 0; i< posicao.size(); i++){
 
-        for ( int cont = 0; cont == 61; cont++) {
-            Peca peca = posicaoPecasTabuleiro.get(cont).getPeca();
-            if (cont == 0 || cont == 8 || cont == 16 || cont == 24 || cont == 32 || cont == 40 || cont == 48 || cont == 56) {
-                System.out.println("|" + peca + "(" + posicaoPecasTabuleiro + ")");
-            }
-            else if (cont == 7 || cont == 15 || cont == 23 || cont == 31 || cont == 39 || cont == 47 || cont == 55 || cont == 63) {
-                System.out.println(peca + "(" + posicaoPecasTabuleiro + ")|");
-            }
-            else{
-                System.out.println("|" + peca + "(" + posicaoPecasTabuleiro + ")|");
-            }
-        }
+          if( posicao != null){
+              Peca peca = posicao.get(i).getPeca();
+              if (i % 8 == 0) {
+
+              }
+          }
+
+      }
     }
 //endregion
 
